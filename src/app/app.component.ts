24cecd0b654a4cbeff2b8232a74d0e821c50e21c
Rodrigo Mastropietro <rodrigo.mastropietro@sap.com>
2020-04-06 14:40:01 -0300

    Some changes introduced to the Belcorp POC.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {BaseSiteService, RoutingService} from '@spartacus/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'spartacus-bootcamp';
+export class AppComponent implements OnInit {
+
+  website: string;
+
+  constructor(private baseSiteService: BaseSiteService) {
+  }
+
+  ngOnInit(): void {
+    this.baseSiteService.getActive().subscribe(
+      data => {
+        if (data === 'electronics-spa') {
+          this.website = 'esika';
+        } else {
+          this.website = "lbel";
+        }
+      }
+    );
+  }
+
 }
